6f15ba3a442ab22b7764e5925d8f62d280591c76
jdboachie <judeboachiekwame@gmail.com>
2024-03-01 21:44:11 +0000

    fix: changed temporal landing ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <main className="w-full h-full justify-center items-center font-mono">
+    <main className="w-screen h-screen flex flex-col justify-center items-center font-mono">
       <p className="">Landing page is under development</p>
-      <Link href={'/dashboard'}>Visit dashboard page</Link>
+      <Link href={'/dashboard'} className="underline">Visit dashboard page</Link>
     </main>
   );
 }
